d3a249b92bfebe09024059218806d179c683ed26
muskankhedia <muskan.khedia2000@gmail.com>
2020-04-12 20:26:42 +0530

    shorten and saved the URL

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
 const Shorten = require('./short/shortener').Shortener;
-const dbManager = require('./utils/db-manager').DBManager;
 
-// Creating a dbManager object
-const db = new dbManager();
-
-// Creating a database to store all the URLs
-db.createDB('./dbs/URLData')
+const shortURL = new Shorten()
 
 app.get('/', function (req, res) {
   res.send('Please send the url to the route "/urlmini" to shorten the URL')
 })
 
-// Declares the route '/urlmini', recieves the basicURL as request
-// Responses the shorten URL
-// Appends the shortened URL with proper indexing in the database
+/**
+ * Returns the shorten URL
+ * Appends the shortened URL with proper indexing in the database
+ * Declares the route '/urlmini', recieves the basicURL as request
+ *
+ */
 app.get('/urlmini', function(req, res) {
-    var shortURL = new Shorten(req.query.query)
-    var outputURL = shortURL.shortenURL()
-    this.data['x'] = { y: outputURL}
-    db.pushData('/a', this.data, false)
+    var outputURL = shortURL.shortenURL(req.query.query)
+    console.log("outputURL:: ", outputURL);
     res.send(outputURL)
 })
 
